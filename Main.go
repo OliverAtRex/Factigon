acde16f4834a0427761482fc2d4da7aae39b8c19
Oliver Byers <oliver@rbyers.net>
2018-11-11 13:55:07 -0500

    first working server!

---
+++ Main.go
@@ -0,0 +1,15 @@
+package main
+import (
+		"net/http"
+		"google.golang.org/appengine"
+)
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.Redirect(w, r, "/", http.StatusFound)
+		return
+	}
+}
+func main() {
+	http.HandleFunc("/", indexHandler)
+	appengine.Main()
+}
